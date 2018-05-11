98095c20f43ea24bc369e17defaaea9bd8b16b05
Mário Lucas <maryosk8bkt@gmail.com>
2018-05-11 18:50:35 -0300

    add model person

---
+++ src/models/personModel.js
@@ -0,0 +1,7 @@
+const persons = [
+    { id: 1, name: 'Mário', idade: '23' },
+    { id: 2, name: 'Henirque', idade: '23' },
+    { id: 3, name: 'Maurício', idade: '20' }
+];
+
+exports.personsGet = () => persons;
