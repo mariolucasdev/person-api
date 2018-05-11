3673cd911871735dc907d8e877c2b4a573cdfa71
Mário Lucas <maryosk8bkt@gmail.com>
2018-05-11 19:17:22 -0300

    create books model

---
+++ src/models/booksModel.js
@@ -0,0 +1,32 @@
+const books = [
+    {
+      id: 'kansdjkn23njknwdj',
+      title: 'books',
+      author: 'Albert Einstain',
+      pages: 148,
+      description: 'descriptions books text'
+    },
+    {
+      id: 1,
+      title: 'books',
+      author: 'Albert Einstain',
+      pages: 148,
+      description: 'descriptions books text'
+    },
+    {
+      id: 2,
+      title: 'books',
+      author: 'Albert Einstain',
+      pages: 148,
+      description: 'descriptions books text'
+    },
+    {
+      id: 3,
+      title: 'books',
+      author: 'Albert Einstain',
+      pages: 148,
+      description: 'descriptions books text'
+    }
+  ];
+
+  exports.getBooks = () => books;
